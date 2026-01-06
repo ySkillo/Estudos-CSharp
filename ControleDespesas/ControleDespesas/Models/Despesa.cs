57a614984a5169e111f96b936d1c89d7252c3e75
ySkillo <matheus.doliveira@sptech.school>
2026-01-06 12:49:16 -0300

    Primeiro projeto de console C#

---
+++ ControleDespesas/ControleDespesas/Models/Despesa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleDespesas.Models
+{
+    public class Despesa
+    {
+        public string Descricao { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public string? Observacao { get; set; } // nullable permitindo valores nulos
+    }
+}
