91477af4030fae835fc5b26ceb950aedd07e4665
yskillo <matheus.doliveira@sptech.school>
2026-01-14 13:04:48 -0300

    estrutura do projeto criada

---
+++ SistemaAssinaturas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemaAssinaturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
